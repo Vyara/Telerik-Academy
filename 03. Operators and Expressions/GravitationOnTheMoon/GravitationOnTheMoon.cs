20e57385c9ed52ba498804e2d0ceb0f01b7c94a9
vyara <vyara_hristova@yahoo.de>
2015-01-21 22:51:54 +0200

    initial commit

---
+++ 03. Operators and Expressions/GravitationOnTheMoon/GravitationOnTheMoon.cs
@@ -0,0 +1,27 @@
+﻿//Problem 2. Gravitation on the Moon
+
+//The gravitational field of the Moon is approximately 17% of that on the Earth.
+//Write a program that calculates the weight of a man on the moon by a given weight on the Earth.
+//Examples:
+
+//weight	weight on the Moon
+//86	    14.62
+//74.6	    12.682
+//53.7	    9.129
+
+
+using System;
+using System.Globalization;
+
+
+class GravitationOnTheMoon
+{
+    static void Main()
+    {
+        Console.Write("Please enter a person's weight in kilograms: ");
+        double earthWeight = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        double moonWeight = earthWeight * 0.17;
+        Console.WriteLine("A person weighing {0} kg on Earth, will weigh {1} kg on the Moon.", earthWeight, moonWeight);
+    }
+}
+
---
+++ 03. Operators and Expressions/ThirdDigitIs7/ThirdDigitIs7.cs
@@ -0,0 +1,41 @@
+﻿//Problem 5. Third Digit is 7?
+
+//Write an expression that checks for given integer if its third digit from right-to-left is 7.
+//Examples:
+
+//n	    Third digit 7?
+//5	        false
+//701	    true
+//9703	    true
+//877	    false
+//777877	false
+//9999799	true
+
+
+using System;
+
+
+
+class ThirdDigitIs7
+{
+    static void Main()
+    {
+        string str;
+        int number;
+        
+        do
+        {
+            Console.Write("Please enter an integer: ");
+            str = Console.ReadLine();
+            Console.Clear();
+
+        } while (!int.TryParse(str, out number));
+
+        bool isThird = ((number / 100) % 10) == 7;
+
+        Console.WriteLine("The third digit of the number {0} is 7: {1}", number, isThird);
+
+
+    }
+}
+
