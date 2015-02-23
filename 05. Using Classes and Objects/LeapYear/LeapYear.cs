ffa45828eec22cca625932e151ccd4c76e4f4412
vyara <vyara_hristova@yahoo.de>
2015-02-24 01:59:58 +0200

    initial commit Using Classes And Objects

---
+++ 05. Using Classes and Objects/LeapYear/LeapYear.cs
@@ -0,0 +1,39 @@
+﻿//Problem 1. Leap year
+
+//Write a program that reads a year from the console and checks whether it is a leap one.
+//Use System.DateTime.
+
+
+using System;
+
+
+class LeapYear
+{
+    static void Main()
+    {
+        string str;
+        int year;
+
+        //asks user to enter a year
+        do
+        {
+            Console.Write("Please enter a year(yyyy): ");
+            str = Console.ReadLine();
+
+        } while (!int.TryParse(str, out year));
+
+        //checks if year is a leap one
+        Console.WriteLine("------------------------------");
+        if (DateTime.IsLeapYear(year))
+        {
+            Console.WriteLine("Year {0} is a leap year.", year);
+        }
+
+        else
+        {
+            Console.WriteLine("Year {0} is NOT a leap year.", year);
+        }
+    }
+
+}
+
---
+++ 05. Using Classes and Objects/Workdays/Workdays.cs
@@ -0,0 +1,64 @@
+﻿//Problem 5. Workdays
+
+//Write a method that calculates the number of workdays between today and given date, passed as parameter.
+//Consider that workdays are all days from Monday to Friday except a fixed list of public holidays specified preliminary as array.
+
+
+using System;
+using System.Globalization;
+using System.Linq;
+
+
+class Workdays
+{
+    static void Main()
+    {
+        string input;
+
+        //asks user for input
+        Console.Write("Please enter a date between today and 31.12.2015(dd-mm-yyyy): ");
+        input = Console.ReadLine();
+        
+        //splits user input and creates an array, then builds a new DateTime from its elements
+        int[] formatInput = input.Split('-').Select(int.Parse).ToArray();
+        DateTime date = new DateTime(formatInput[2], formatInput[1], formatInput[0]);
+        
+        //calculates result using NumberOfWorkDays()
+        int result = NumberOfWorkDays(date);
+        
+        //prints the result
+        Console.WriteLine("-----------------------------------");
+        Console.WriteLine("There are {0} working days between {1:dd/MM/yyyy} and {2:dd/MM/yyyy}", result, DateTime.Now, date);  
+
+    }
+    //method that calculates the number of workdays between today and given date
+    static int NumberOfWorkDays(DateTime date)
+    {
+        DateTime today = DateTime.Now;
+        DateTime[] officialHoliays = {new DateTime(2015, 1, 1),  new DateTime(2015, 3, 3), new DateTime(2015, 4, 10), 
+                                      new DateTime(2015, 4, 13), new DateTime(2015, 5, 1), new DateTime(2015, 5, 6),
+                                      new DateTime(2015, 5, 24), new DateTime(2015, 9, 6), new DateTime(2015, 9, 22),
+                                      new DateTime(2015, 12, 24), new DateTime(2015, 12, 25)};
+        int workingDays = 0;
+        if (today > date)
+        {
+            DateTime temp = today;
+            date = temp;
+            today = date;
+        }
+
+        while (today <= date)
+        {
+            if (!officialHoliays.Contains(today) && today.DayOfWeek != DayOfWeek.Saturday && today.DayOfWeek != DayOfWeek.Sunday)
+            {
+                workingDays++;
+            }
+            today = today.AddDays(1);
+        }
+
+        return workingDays;
+    }
+
+
+}
+
