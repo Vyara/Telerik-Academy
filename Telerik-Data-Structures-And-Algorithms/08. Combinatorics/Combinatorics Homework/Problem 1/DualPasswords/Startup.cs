9d83b4ebdc3a36bcfcea16c182734485c2504acb
vyara <vyara_hristova@yahoo.de>
2015-11-17 19:09:30 +0200

    initial commit

---
+++ Telerik-Data-Structures-And-Algorithms/08. Combinatorics/Combinatorics Homework/Problem 1/DualPasswords/Startup.cs
@@ -0,0 +1,30 @@
+﻿namespace DualPasswords
+{
+    using System;
+
+    public class Startup
+    {
+        public static void Main()
+        {
+            var password = Console.ReadLine();
+            int unknownDigits = 0;
+
+            for (int i = 0; i < password.Length; i++)
+            {
+                if (password[i] == '*')
+                {
+                    unknownDigits++;
+                }
+            }
+
+            ulong result = 1;
+
+            for (int i = 1; i <= unknownDigits; i++)
+            {
+                result <<= 1;
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ Telerik-Data-Structures-And-Algorithms/08. Combinatorics/Combinatorics Homework/Problem 2/ColorfulRabbits/Startup.cs
@@ -0,0 +1,46 @@
+﻿namespace ColorfulRabbits
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class Startup
+    {
+        public static void Main()
+        {
+            Dictionary<int, int> rabbits = new Dictionary<int, int>(1000000);
+            int numberOfRabbits = 0;
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                int rabbit = int.Parse(Console.ReadLine());
+
+                if (rabbits.ContainsKey(rabbit))
+                {
+                    int val = rabbits[rabbit];
+                    if (val > rabbit)
+                    {
+                        numberOfRabbits += rabbit + 1;
+                        rabbits.Remove(rabbit);
+                    }
+                    else
+                    {
+                        rabbits[rabbit] += 1;
+                        if (val == rabbit)
+                        {
+                            rabbits.Remove(rabbit);
+                        }
+                    }
+                }
+                else
+                {
+                    rabbits.Add(rabbit, 1);
+                    numberOfRabbits += rabbit + 1;
+                }
+            }
+
+            Console.WriteLine(numberOfRabbits);
+        }
+    }
+}
